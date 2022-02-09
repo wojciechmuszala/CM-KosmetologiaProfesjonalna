0f6da4bf9467be2a29d3c8ad81419b1a88d30210
Wojciech Muszala <wojciech.muszala@gmail.com>
2022-02-09 22:46:19 +0100

    Modify section offer (add price list), repair side-menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 const burger = document.querySelector('.burger');
 const sideMenu = document.querySelector('.side-menu div');
 
-// function showMenu() {
-//     console.log('klik')
-//     sideMenu.classList.toggle('lol');
-// }
-
-// burger.addEventListener('click',showMenu());
-
 burger.addEventListener('click', function () {
     sideMenu.classList.toggle('show')
 })
