509a222f38363448f70341936068c8bc569a4c7b
Wojciech Muszala <wojciech.muszala@gmail.com>
2022-02-10 23:21:35 +0100

    Modify section offer (ul -> table). Add some @media

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector('.burger');
 const sideMenu = document.querySelector('.side-menu div');
+const offer = document.querySelector('.offer-photo')
+const priceList = document.querySelector('.price-list')
 
 burger.addEventListener('click', function () {
     sideMenu.classList.toggle('show')
