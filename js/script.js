b314fe8f0ea970c5ea9dcc91c3f5ab18bcb0ffb8
Wojciech Muszala <wojciech.muszala@gmail.com>
2022-02-12 16:16:54 +0100

    Add opening hours, restyle section offer, add some @media

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const burger = document.querySelector('.burger');
 const sideMenu = document.querySelector('.side-menu div');
-const offer = document.querySelector('.offer-photo')
-const priceList = document.querySelector('.price-list')
+const menuItem = document.querySelectorAll('.side-menu a')
+const offerItem = document.querySelectorAll('.offer div.offer-photo')
+const priceList = document.querySelector('.offer div.price-list')
 
 burger.addEventListener('click', function () {
     sideMenu.classList.toggle('show')
 })
+
+menuItem.forEach((menuItem) => {
+    menuItem.addEventListener('click', function () {
+        sideMenu.classList.remove('show')
+        console.log('klik-menu')
+    })
+
+})
+
+// offerItem.forEach((offerItem) => {
+//     offerItem.addEventListener('click', function () {
+//         priceList.classList('show')
+//         console.log('klik-offer')
+
+//     })
+// })
